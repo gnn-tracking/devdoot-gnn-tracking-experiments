5bf130bb601ab800c040c2d4b005a2178c8d1a4e
Devdoot Chatterjee <chatterjeedevdoot@gmail.com>
2023-07-26 07:34:54 -0400

    early stopping

--- experiments/run_graph_construction.py
+++ experiments/run_graph_construction.py
@@ -5,14 +5,30 @@ from gnn_tracking.utils.loading import TrackingDataModule
 from gnn_tracking.utils.nomenclature import random_trial_name
 from gnn_tracking.training.callbacks import PrintValidationMetrics
 
-from pytorch_lightning.callbacks import TQDMProgressBar, RichProgressBar
+from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint, RichProgressBar
 from pytorch_lightning.cli import LightningCLI
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 from pytorch_lightning.plugins.environments import SLURMEnvironment
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from wandb_osh.lightning_hooks import TriggerWandbSyncLightningCallback
+
 
 name = random_trial_name()
 
+early_stopping_callback = EarlyStopping(
+    monitor='n_edges_frac_segment50_95',
+    patience=5,
+    mode='min',
+    check_finite=False
+)
+
+checkpoint_callback = ModelCheckpoint(
+    filename='{epoch}-{n_edges_frac_segment50_95:.2f}',
+    monitor='n_edges_frac_segment50_95',
+    mode='min',
+    save_top_k=1,
+    save_last=True
+)
 
 logger = WandbLogger(
     project="mdmm",
@@ -30,6 +46,8 @@ def cli_main():
         datamodule_class=TrackingDataModule,
         trainer_defaults={
             "callbacks": [
+                early_stopping_callback,
+                checkpoint_callback,
                 PrintValidationMetrics(),
                 TQDMProgressBar(refresh_rate=5),
                 TriggerWandbSyncLightningCallback(),
